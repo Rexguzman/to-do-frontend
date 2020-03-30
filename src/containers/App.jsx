898fef6702f6d894b06bc63119c0c75fe8c3a829
Rex Guzman <rexguzman1@gmail.com>
2020-03-30 19:49:34 -0400

    Agregando archivos faltantes

---
+++ src/containers/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import HelloWorld from '../components/HelloWorld.jsx'
+
+import '../assets/styles/App.css'
+
+const App = () => (
+    <div className="App">
+        <HelloWorld/>
+    </div>
+);
+
+export default App;
